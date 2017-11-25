80670e3dde4e2fda4807c542204d6c9eadfaa630
Guntereno <GunterPete@hotmail.com>
2017-11-25 14:20:50 +0000

    Replaced LCD controls with backlight

--- layouts/community/ergodox/guntereno/keymap.c
+++ layouts/community/ergodox/guntereno/keymap.c
@@ -119,9 +119,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
 
         // Right hand side0
         KC_NO,   KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   RESET,
-        KC_NO,   KC_NO, RGB_TOG, RGB_VAI, RGB_SLD, KC_NO,   EPRM,
-        KC_NO,   KC_NO, RGB_HUD, RGB_VAD, RGB_HUI, KC_NO,
-        KC_TRNS, KC_NO, KC_NO,   RGB_MOD, KC_NO,   KC_NO,   KC_TRNS,
+        KC_NO,   KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   EPRM,
+                 KC_NO, BL_TOGG, BL_DEC,  BL_INC,  KC_NO,   KC_NO,
+        KC_TRNS, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS,
                         KC_NO,   KC_NO,   KC_TRNS, KC_TRNS, KC_TRNS,
 
         KC_NO, KC_NO,
