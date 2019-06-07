8896fde52f71614ab96e06f79a1b34afa51ff79a
Guntereno <GunterPete@hotmail.com>
2019-06-07 09:56:55 +0100

    Swapped alt/windows keys back

--- layouts/community/ergodox/guntereno/keymap.c
+++ layouts/community/ergodox/guntereno/keymap.c
@@ -41,18 +41,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
         KC_NUHS,  KC_Q,    KC_W,    KC_E,    KC_R,     KC_T, KC_NUBS,
         KC_TAB,   KC_A,    KC_S,    KC_D,    KC_F,     KC_G,
         KC_LSPO,  KC_Z,    KC_X,    KC_C,    KC_V,     KC_B, MO(SPECIAL),
-        CK_LCTR,  KC_LALT, KC_LGUI, KC_LEFT, KC_RIGHT,
+        CK_LCTR,  KC_LGUI, KC_LALT, KC_LEFT, KC_RIGHT,
 
                  KC_GRV, KC_HOME,
                          KC_END,
         KC_BSPC, KC_DEL, MO(FUNCTION),
 
         // Right hand side
-        TG(NUMPAD),   KC_6, KC_7,  KC_8,    KC_9,           KC_0,    KC_MINS,
-        KC_LBRACKET,  KC_Y, KC_U,  KC_I,    KC_O,           KC_P,    KC_RBRACKET,
-                      KC_H, KC_J,  KC_K,    KC_L,           KC_SCLN, KC_QUOT,
-        MO(SPECIAL),  KC_N, KC_M,  KC_COMM, KC_DOT,         KC_SLSH, KC_RSPC,
-                            KC_UP, KC_DOWN, KC_APPLICATION, KC_RALT, CK_RCTR,
+        TG(NUMPAD),   KC_6, KC_7,  KC_8,    KC_9,    KC_0,           KC_MINS,
+        KC_LBRACKET,  KC_Y, KC_U,  KC_I,    KC_O,    KC_P,           KC_RBRACKET,
+                      KC_H, KC_J,  KC_K,    KC_L,    KC_SCLN,        KC_QUOT,
+        MO(SPECIAL),  KC_N, KC_M,  KC_COMM, KC_DOT,  KC_SLSH,        KC_RSPC,
+                            KC_UP, KC_DOWN, KC_RALT, KC_APPLICATION, CK_RCTR,
 
         KC_PGUP,      TD(TD_PAUSE_PRNTSCRN),
         KC_PGDOWN,
